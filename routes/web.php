40dde4068066ca03640f12bc55eadeef6c6623be
BagusFary <bagusfaryananta@gmail.com>
2023-07-26 22:39:01 +0700

    Authentication Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,9 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', function(){
-    return view('index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/post/show/{id}',[PostController::class, 'show']);
-Route::get('/post/edit/{id}',[PostController::class, 'edit']);
+require __DIR__.'/auth.php';
